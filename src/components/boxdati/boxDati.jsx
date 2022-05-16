b6867dff94ee46beeeb31a33377c0fa03e8857cf
grafica <paolo@monema.it>
2022-05-16 18:20:16 +0200

    animazione nel change della props

--- src/components/boxdati/boxDati.jsx
+++ src/components/boxdati/boxDati.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import style from './boxDati.module.css'
 import iconaOk from '../../assets/images/green-led.svg'
+import { motion, AnimatePresence } from "framer-motion"
 
 function StatoOk() {
   return (
@@ -10,18 +12,40 @@ function StatoOk() {
       </div>
       <div className={style.datiStato}>
         <div className={style.label}>OK</div>
-        <div className={style.datoLabel}>sensore funzionante</div>
+        <div className={style.datoLabel}>(sensore funzionante)</div>
       </div>
     </div>
   )
 }
 
-function BloccoNumerico ({datiNumerici}) {
-  return(
-    <div className={style.bloccoDati}>
-      <div className={style.titoloInterno}>{datiNumerici.titolo}</div>
-      <div className={style.datoInterno}>{datiNumerici.dato}</div>
-    </div>
+function BloccoNumerico({ datiNumerici, code, index }) {
+  const [animate, setAnimate] = useState(true)
+
+  useEffect(() => {
+    setAnimate(false) 
+
+    setTimeout(() => {
+      setAnimate(true)
+    }, 700)
+
+  }, [code])
+
+  return (
+    <>
+      
+  <motion.div
+          key={code}
+          className={style.bloccoDati}
+          initial={{ opacity: 0, top:50 }}
+          animate={{ opacity: 1, top:0 }}
+          exit={{ opacity: 0, top: 50  }}
+          transition={{duration:.6, delay: index * 0.1}}
+        >
+          <div className={style.titoloInterno}>{datiNumerici.titolo}</div>
+          <div className={style.datoInterno}>{datiNumerici.dato} <span>%</span></div>
+        </motion.div>
+     
+    </>
   )
 }
 
@@ -36,9 +60,15 @@ function BoxDati({ dati }) {
         <div className={style.subData}>
           <StatoOk />
         </div>
-   {/*      <div className={style.datiInterni}>
-        {dati.datiInterni && dati.datiInterni.map((item)=> <BloccoNumerico datiNumerici ={item} />) }
-        </div> */}
+        <div className={style.datiInterni}>
+          {dati?.datiInterni?.map((item, index) =>
+
+            <React.Fragment key={item.titolo}>
+              <BloccoNumerico datiNumerici={item} code={dati.code} index={index} />
+            </React.Fragment >
+
+          )}
+        </div>
       </header>
     </div>
   )
---
+++ src/components/header/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from './header.module.css'
+function Header() {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
