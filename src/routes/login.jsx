bb353730eab318ce10964fb7f73cdadc388e6477
TheRealLorenz <lollobellina@gmail.com>
2022-07-13 15:31:05 +0200

    Reso decente il login

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,10 +1,13 @@
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import AuthService from '../services/auth.service';
+import './login.scss';
+import '../components/ui/ui.css';
 
 
 export default function Login() {
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("empty");
+  const [isErrorHidden, setErrorHidden] = useState(true);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [userData, setUserData] = useState(AuthService.getUserData());
@@ -17,7 +20,8 @@ export default function Login() {
       })
       .catch((error) => {
         setErrorMessage(error.response.data.message);
-        setTimeout(() => setErrorMessage(""), 2000);
+        setErrorHidden(false);
+        setTimeout(() => setErrorHidden(true), 2000);
       });
   }
 
@@ -36,14 +40,18 @@ export default function Login() {
   }, [userData, navigate]);
 
   return (
-    <>
-      <p>{errorMessage}</p>
-      <main>
-        <p>Login Page!</p>
-        <input onChange={handleEmailChange} />
-        <input onChange={handlePasswordChange} />
-        <button onClick={() => login(email, password)}>Login</button>
-      </main>
-    </>
+    <div className='login-root'>
+      <div className={`error-message ${isErrorHidden ? 'hidden' : ''}`}>{errorMessage}</div>
+      <div className='login-panel'>
+        <h1 className='login-title'>Login Page!</h1>
+        <div className='input-div'>
+          <label for='email'>Email</label>
+          <input id='email' onChange={handleEmailChange} />
+          <label for='password'>Password</label>
+          <input type='password' id='password' onChange={handlePasswordChange} />
+        </div>
+        <button className='login-button' onClick={() => login(email, password)}>Login</button>
+      </div>
+    </div>
   );
 }
