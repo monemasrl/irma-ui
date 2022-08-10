c2ac86dd962175aaf546c5fc251497b0c0bac55c
TheRealLorenz <lollobellina@gmail.com>
2022-08-10 12:04:37 +0200

    Fix: mock del login

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -4,10 +4,11 @@ const MOCK_LOGIN = process.env.REACT_APP_MOCK_LOGIN || 0;
 
 const login = async (username, password) => {
   if (MOCK_LOGIN) {
+    localStorage.setItem("jwt-token", "1234foobar");
     return "1234foobar";
   }
 
-  const response = Microservice.authenticate(username, password)
+  const response = await Microservice.authenticate(username, password)
   localStorage.setItem("jwt-token", response.data?.access_token);
   return response.data?.access_token;
 }
