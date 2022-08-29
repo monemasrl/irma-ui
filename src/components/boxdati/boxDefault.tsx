928355f4f43b9d2d2c7536589147cf70627554df
TheRealLorenz <lollobellina@gmail.com>
2022-08-29 12:45:11 +0200

    Migrazione boxDefault.jsx in tsx

--- src/components/boxdati/boxDefault.tsx
+++ src/components/boxdati/boxDefault.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { FC } from 'react';
 import style from './boxDati.module.scss';
 import { motion } from 'framer-motion';
+import { StakerDefaultData } from '../../App';
 
-function BoxDefault({ datiDefault }) {
+type Props = {
+  datiDefault: StakerDefaultData;
+};
+
+const BoxDefault: FC<Props> = ({ datiDefault }) => {
   return (
     <motion.header
-      key={datiDefault}
+      key={JSON.stringify(datiDefault)}
       initial={{ opacity: 0, top: 20, position: 'relative' }}
       animate={{ opacity: 1, top: 0, position: 'relative' }}
       exit={{ opacity: 0, top: 20 }}
@@ -15,16 +20,14 @@ function BoxDefault({ datiDefault }) {
         <li>
           <div className={style.title}>
             <div className={style.titoletto}>Reach Staker Totali</div>
-            <div className={style.codiceStaker}>
-              {datiDefault?.numeroStaker}
-            </div>
+            <div className={style.codiceStaker}>{datiDefault.numeroStaker}</div>
           </div>
         </li>
         <li>
           <div className={style.title}>
             <div className={style.titoletto}>Ore operative totali</div>
             <div className={style.codiceStaker}>
-              {datiDefault?.oreOperativeTotali}
+              {datiDefault.oreOperativeTotali}
             </div>
           </div>
         </li>
@@ -32,13 +35,13 @@ function BoxDefault({ datiDefault }) {
           <div className={style.title}>
             <div className={style.titoletto}>Allerte</div>
             <div className={style.codiceStaker}>
-              {datiDefault?.allerteAttuali}
+              {datiDefault.allerteAttuali}
             </div>
           </div>
         </li>
       </ul>
     </motion.header>
   );
-}
+};
 
 export default BoxDefault;
