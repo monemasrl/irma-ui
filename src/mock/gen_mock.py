764449c521ebb925f22e6179dafe5e8963d5154e
TheRealLorenz <lollobellina@gmail.com>
2022-09-22 15:20:31 +0200

    Fix sessionID and readingID in mock

--- src/mock/gen_mock.py
+++ src/mock/gen_mock.py
@@ -7,31 +7,35 @@ BASE_TIMESTAMP = 1_641_016_800
 
 readings = []
 
-# readingID
-for k in range(4 * 2):
-    # nodeID
-    for p in range(6):
-        # canID
-        for x in [1, 2, 3, 4]:
-            dangerLevel = randint(0, 9)
-            # sensorNumber
-            for y in [1, 2]:
-                sessionID = k % 2
-                timedelta = k * 100 + p * 5 + x * 2 + y
+# sessionIDnum
+for s in range(2):
+    # readingIDnum
+    for k in range(4):
+        # nodeID
+        for p in range(6):
+            # canID
+            for x in [1, 2, 3, 4]:
+                dangerLevel = randint(0, 9)
+                # sensorNumber
+                for y in [1, 2]:
+                    timedelta = s * 300 + k * 30 + p * 5 + x * 2 + y
 
-                reading = {
-                    "nodeID": p,
-                    "canID": x,
-                    "sensorNumber": y,
-                    "readingID": k,
-                    "sessionID": sessionID,
-                    "dangerLevel": dangerLevel,
-                    "window1Count": randint(0, 10_000_000),
-                    "window2Count": randint(0, 10_000_000),
-                    "window3Count": randint(0, 10_000_000),
-                    "publishedAt": datetime.fromtimestamp(BASE_TIMESTAMP + timedelta).isoformat()
-                }
+                    sessionID = s * 300 + BASE_TIMESTAMP
+                    readingID = s * 300 + k * 30 + BASE_TIMESTAMP
 
-                readings.append(reading)
+                    reading = {
+                        "nodeID": p,
+                        "canID": x,
+                        "sensorNumber": y,
+                        "readingID": readingID,
+                        "sessionID": sessionID,
+                        "dangerLevel": dangerLevel,
+                        "window1Count": randint(0, 10_000_000),
+                        "window2Count": randint(0, 10_000_000),
+                        "window3Count": randint(0, 10_000_000),
+                        "publishedAt": datetime.fromtimestamp(BASE_TIMESTAMP + timedelta).isoformat()
+                    }
+
+                    readings.append(reading)
 
 print(json.dumps(readings))
