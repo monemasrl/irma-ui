ef7e9421593a96799480ab3ba76dd51bbcdcb9cb
Andrea Bettarini <bettarini@monema.it>
2023-01-17 17:16:40 +0100

    Aggiunto Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Login from './routes/login';
+import * as Sentry from '@sentry/react';
 import { UserContextProvider } from './context/user-context';
 import reportWebVitals from './reportWebVitals';
+import { BrowserTracing } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: 'https://6d10681d40974885b2f71731d833ba98@o4504445689331712.ingest.sentry.io/4504520707866624',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 root.render(
